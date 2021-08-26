d6811bd8e01a3f7ffd582e566d356b44f3fb5fba
krayevsky <heartoscope@gmail.com>
2021-08-26 16:01:34 -0600

    Done.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {            
-    res.sendFile(path.join(__dirname, "../index.html"));  
+    res.sendFile(path.join(__dirname, "../index.html"));
+});
+
+app.get("/css", (req, res) => {            
+    res.sendFile(path.join(__dirname, "../styles.css"));
 });
 
 const port = process.env.PORT || 4005;
